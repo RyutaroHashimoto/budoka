9cb35dc7fe887e1549129e66719ee749b5c001e9
Ryutaro Hashimoto <ryutaro@umich.edu>
2023-10-02 22:49:48 -0400

    add: data

--- src/project/src/project/pipelines/data_processing/pipeline.py
+++ src/project/src/project/pipelines/data_processing/pipeline.py
@@ -4,25 +4,4 @@ from .nodes import create_model_input_table, preprocess_companies, preprocess_sh
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
-        [
-            node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
-            ),
-            node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
-            ),
-            node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
-            ),
-        ]
-    )
+    return pipeline([])
